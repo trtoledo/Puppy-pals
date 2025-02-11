aa3ae693a80ae72de421ba4847403267ba818fef
Tomas Toledo <trtoledo1977@gmail.com>
2025-02-11 00:00:46 -0300

    app module css

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,44 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import {puppyList} from './data.js'
+import styles from './App.module.css';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
 
   return (
-    <>
-      <div>
+    <div className={styles['appContainer']}>
+      <div className={styles['puppy-list']}>
+        <h1>Puppy Pals</h1>
+        <h2>Click on a puppy to see more info</h2>
+        {puppies.map((puppy) => (
+          <p
+            onClick={() => setFeatPupId(puppy.id)}
+            key={puppy.id}
+            className={styles['puppy']}
+          >
+            {puppy.name}
+          </p>
+        ))}
+      </div > {featPupId && (
+        <div className={styles['featured']}>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>
+              <span>Age: {featuredPup.age}</span>
+            </li>
+            <li>
+              <span>Email: {featuredPup.email}</span>
+            </li>
+          </ul>
+        </div>
+      )}</div>
 
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  );
 }
 
 export default App
